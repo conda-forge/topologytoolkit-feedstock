0318fd446608e0b98cb2272fcbea0147fb3b996b
lhofmann <lhofmann@users.noreply.github.com>
2020-12-22 17:18:08 +0100

    Update to v0.9.9

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,8 @@ cd build
 
 if [[ ${HOST} =~ .*linux.* ]]; then
     # temporary workaround for issue https://github.com/conda-forge/vtk-feedstock/issues/86
-    find ${PREFIX}/lib/cmake/vtk-8.2/ -type f -print0 | xargs -0 \
-        sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib.*;##g'
+    # find ${PREFIX}/lib/cmake/vtk-8.2/ -type f -print0 | xargs -0 \
+    #     sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib.*;##g'
 fi
 
 cmake .. -G "Ninja" \
@@ -17,9 +17,9 @@ cmake .. -G "Ninja" \
     -DTTK_BUILD_VTK_WRAPPERS=ON \
     -DTTK_BUILD_VTK_PYTHON_MODULE=ON \
     -DTTK_BUILD_PARAVIEW_PLUGINS=OFF \
-    -DTTK_BUILD_STANDALONE_APPS=OFF \
+    -DTTK_BUILD_STANDALONE_APPS=ON \
     -DTTK_ENABLE_CPU_OPTIMIZATION=OFF \
-    -DTTK_ENABLE_KAMIKAZE=OFF \
+    -DTTK_ENABLE_KAMIKAZE=ON \
     -DTTK_ENABLE_MPI=OFF \
     -DTTK_ENABLE_OPENMP=ON
 
