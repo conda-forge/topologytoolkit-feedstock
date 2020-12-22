7f1b6f2472cca356ce4759701e255bd37060f42e
lhofmann <lhofmann@users.noreply.github.com>
2020-12-22 17:18:46 +0100

    Do not use ninja (see https://github.com/topology-tool-kit/ttk/issues/298)

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,13 @@
 mkdir -p build
 cd build
 
-if [[ ${HOST} =~ .*linux.* ]]; then
-    # temporary workaround for issue https://github.com/conda-forge/vtk-feedstock/issues/86
-    # find ${PREFIX}/lib/cmake/vtk-8.2/ -type f -print0 | xargs -0 \
-    #     sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib.*;##g'
-fi
+#if [[ ${HOST} =~ .*linux.* ]]; then
+#    # temporary workaround for issue https://github.com/conda-forge/vtk-feedstock/issues/86
+#    find ${PREFIX}/lib/cmake/vtk-8.2/ -type f -print0 | xargs -0 \
+#        sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib.*;##g'
+#fi
 
-cmake .. -G "Ninja" \
+cmake .. \
     -Wno-dev \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH:PATH="${PREFIX}" \
@@ -23,4 +23,4 @@ cmake .. -G "Ninja" \
     -DTTK_ENABLE_MPI=OFF \
     -DTTK_ENABLE_OPENMP=ON
 
-ninja install -v
+cmake --build . --target install -- -j"${CPU_COUNT}"
